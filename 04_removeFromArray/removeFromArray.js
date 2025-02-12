8c161cd327fc94a86a69a8225cc613c77f4fca22
mustafa-bhatti <95363241+mustafa-bhatti@users.noreply.github.com>
2025-02-13 00:45:49 +0500

    Complete 04 Remove from Array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(array) {
+    let newArray = [];
+    let indices = [...arguments].slice(1);
+    for (let i = 0;i < array.length;i++) {
+        if (indices.includes(array[i]) == false) {
+            newArray.push(array[i]);
+        }
+        }
+    return newArray;
 };
 
-// Do not edit below this line
+removeFromArray([1,2,3,4],3)
+    // Do not edit below this line
 module.exports = removeFromArray;
